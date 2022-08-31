914604ddc232921911e97583ddfeabf1365161dd
Kevin Mejía <kevinarley6958@gmail.com>
2022-08-30 19:59:55 -0500

    add material tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,35 +1,34 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
-    ],
-    theme: {
-      extend: {
-        backgroundColor: {
-          mainBgColor: "#FCFCFC",
-          greyColor: "#EEEEEE",
-          primary: "#D93939",
-          secondary: "#333333",
-        },
-        textColor: {
-          mainBgColor: "#FCFCFC",
-          greyColor: "#EEEEEE",
-          primary: "#D93939",
-          secondary: "#333333",
-        },
-        colors: {
-          mainBgColor: "#FCFCFC",
-          greyColor: "#EEEEEE",
-          primary: "#D93939",
-          secondary: "#333333",
-        },
-        backgroundImage: {
-          loginWaves: "url('../src/assets/img/wave.svg')",
-        },
+
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        mainBgColor: "#FCFCFC",
+        greyColor: "#EEEEEE",
+        primary: "#D93939",
+        secondary: "#333333",
+      },
+      textColor: {
+        mainBgColor: "#FCFCFC",
+        greyColor: "#EEEEEE",
+        primary: "#D93939",
+        secondary: "#333333",
+      },
+      colors: {
+        mainBgColor: "#FCFCFC",
+        greyColor: "#EEEEEE",
+        primary: "#D93939",
+        secondary: "#333333",
+      },
+      backgroundImage: {
+        loginWaves: "url('../src/assets/img/wave.svg')",
       },
     },
-    plugins: [require('flowbite/plugin')],
-  }
-  
+  },
+  plugins: [],
+});
