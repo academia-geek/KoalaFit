7b4c6e7336121e92fc7ac0f2c4b5f05dffafe115
Nuthelk <nuthelkcd@gmail.com>
2022-09-06 19:01:47 -0500

    intentando arreglar bug merge felipe

--- src/Components/DashboardRetos.jsx
+++ src/Components/DashboardRetos.jsx
@@ -29,7 +29,7 @@ import { challenges } from "../Data/challenges";
 
 const DashboardRetos = () => {
     const user = useSelector((state) => state.login)
-    const retosObject = {challenges}
+    const retosObject = { challenges }
     const idUser = localStorage.getItem("idUserLogin")
     const [aux, setAux] = useState(false)
     const [data, setData] = useState('');
@@ -43,24 +43,24 @@ const DashboardRetos = () => {
     });
 
     useEffect(() => {
-        
-        
+
+
         const calldata = async () => {
-            const prueba =  doc(db, "challenge", idUser)
+            const prueba = doc(db, "challenge", idUser)
             const prueba2 = await getDoc(prueba)
-            
+
             setData(prueba2.data() && prueba2.data().challenges)
-            if(!data){
+            if (!data) {
                 retosDefecto()
             }
         };
 
         calldata();
-        
-        
+
+
     }, [aux]);
 
-    const retosDefecto = async() =>{
+    const retosDefecto = async () => {
         const uid = idUser
         await setDoc(doc(db, 'challenge', uid), retosObject)
     }
@@ -77,12 +77,12 @@ const DashboardRetos = () => {
 
         }
 
-        let challenges = (data != null ? data.concat(datas): datas)
-        
-        const datas2 = { 
+        let challenges = (data != null ? data.concat(datas) : datas)
+
+        const datas2 = {
             challenges
         }
-        
+
 
         const vari = await setDoc(doc(db, 'challenge', uid), datas2)
         setAux(!aux)
@@ -95,7 +95,7 @@ const DashboardRetos = () => {
 
     const handleDelete = ({ target }) => {
         const index = data.filter(e => e.uid !== target.id)
-        
+
         // const array = [index]
         Swal.fire({
             title: 'Are you sure?',
@@ -123,53 +123,53 @@ const DashboardRetos = () => {
 
     return (
         <>
-        <div className="flex flex-col lg:flex-row justify-around gap-10">
-            
-            <div className="bg-white shadow-md rounded-2xl py-8 px-8 lg:h-full max-w-xs m-auto items-center divTable">
-                <h1 className="text-center mb-5 font-bold text-gray-800">
-                    Add Challenge
-                </h1>
-                <form onSubmit={handleSubmit} className="flex flex-col items-center">
-                    <Stack spacing={5}>
-                        <Input
-                            onChange={handleInputChangeName}
-                            name="name"
-                            focusBorderColor="teal.400"
-                            placeholder="Name"
-                        />
-                        <Input
-                            onChange={handleInputChangeName}
-                            name="totalCalories"
-                            focusBorderColor="teal.400"
-                            placeholder="Total calories"
-                        />
-                        <Input
-                            onChange={handleInputChangeName}
-                            name="totalTime"
-                            focusBorderColor="teal.400"
-                            placeholder="Time"
-                        />
-                        <Button colorScheme="green" type="submit" className="">
-                            Add
-                        </Button>
-                    </Stack>
-                </form>
-            </div>
+            <div className="flex flex-col lg:flex-row justify-around gap-10">
 
-            <div className="bg-white shadow-md rounded-2xl lg:h-[600px] lg:w-[1000px] w-full overflow-y-scroll pt-4 mb-8 m-auto divTable">
-                <TableContainer >
-                    <Table size="lg" variant="simple">
-                        <Thead>
-                            <Tr>
-                                <Th>Name</Th>
-                                <Th>Total calories</Th>
-                                <Th>Time</Th>
-                                <Th>Play</Th>
-                                <Th>Editar Time</Th>
-                                <Th>Delete</Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
+                <div className="bg-white shadow-md rounded-2xl py-8 px-8 lg:h-full max-w-xs m-auto items-center divTable">
+                    <h1 className="text-center mb-5 font-bold text-gray-800">
+                        Add Challenge
+                    </h1>
+                    <form onSubmit={handleSubmit} className="flex flex-col items-center">
+                        <Stack spacing={5}>
+                            <Input
+                                onChange={handleInputChangeName}
+                                name="name"
+                                focusBorderColor="teal.400"
+                                placeholder="Name"
+                            />
+                            <Input
+                                onChange={handleInputChangeName}
+                                name="totalCalories"
+                                focusBorderColor="teal.400"
+                                placeholder="Total calories"
+                            />
+                            <Input
+                                onChange={handleInputChangeName}
+                                name="totalTime"
+                                focusBorderColor="teal.400"
+                                placeholder="Time"
+                            />
+                            <Button colorScheme="green" type="submit" className="">
+                                Add
+                            </Button>
+                        </Stack>
+                    </form>
+                </div>
+
+                <div className="bg-white shadow-md rounded-2xl lg:h-[600px] lg:w-[1000px] w-full overflow-y-scroll pt-4 mb-8 m-auto divTable">
+                    <TableContainer >
+                        <Table size="lg" variant="simple">
+                            <Thead>
+                                <Tr>
+                                    <Th>Name</Th>
+                                    <Th>Total calories</Th>
+                                    <Th>Time</Th>
+                                    <Th>Play</Th>
+                                    <Th>Editar Time</Th>
+                                    <Th>Delete</Th>
+                                </Tr>
+                            </Thead>
+                            <Tbody>
                                 {data &&
                                     data.map(({ name, totalCalories, totalTime, uid }, idx) => (
                                         <Tr key={idx} id={uid}>
@@ -177,7 +177,7 @@ const DashboardRetos = () => {
                                             <Td>{totalCalories}</Td>
                                             <Td>{totalTime}</Td>
                                             <Td>
-                                                <Button colorScheme="green" onClick={(totalTime) => {modalTimer.onOpen(); click(totalTime)}}>Go</Button>
+                                                <Button colorScheme="green" onClick={(totalTime) => { modalTimer.onOpen(); click(totalTime) }}>Go</Button>
                                             </Td>
                                             <Td>
                                                 <Button colorScheme="yellow" >Editar</Button>
@@ -187,44 +187,44 @@ const DashboardRetos = () => {
                                             </Td>
                                         </Tr>
                                     ))}
-                        </Tbody>
-                    </Table>
-                </TableContainer>
+                            </Tbody>
+                        </Table>
+                    </TableContainer>
+                </div>
             </div>
-        </div>
-        <Modal
-        isOpen={modalTimer.isOpen}
-        onClose={modalTimer.onClose}
-        finalFocusRef={modalBtn}
-        initialFocusRef={modalBtn}
-        isCentered
-      >
-        <ModalOverlay />
-
-        <ModalContent>
-          <ModalHeader>Edit Profile</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Stack spacing={4}>
-            <ProgressTimer countdownTimestampMs ={counter} />
-            </Stack>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button
-              colorScheme="red"
-              variant="ghost"
-              mr={3}
-              onClick={modalTimer.onClose}
+            <Modal
+                isOpen={modalTimer.isOpen}
+                onClose={modalTimer.onClose}
+                finalFocusRef={modalBtn}
+                initialFocusRef={modalBtn}
+                isCentered
             >
-              Close
-            </Button>
-            <Button colorScheme="green" type="submit">
-              Done
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+                <ModalOverlay />
+
+                <ModalContent>
+                    <ModalHeader>Edit Profile</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Stack spacing={4}>
+                            <ProgressTimer countdownTimestampMs={counter} />
+                        </Stack>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Button
+                            colorScheme="red"
+                            variant="ghost"
+                            mr={3}
+                            onClick={modalTimer.onClose}
+                        >
+                            Close
+                        </Button>
+                        <Button colorScheme="green" type="submit">
+                            Done
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
     );
 };
