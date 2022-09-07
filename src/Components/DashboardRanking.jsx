6136e50309007d929374db91a796078464ceee19
Nuthelk <nuthelkcd@gmail.com>
2022-09-07 14:39:58 -0500

    info ranking sin el emblema listo

--- src/Components/DashboardRanking.jsx
+++ src/Components/DashboardRanking.jsx
@@ -20,7 +20,8 @@ const DashboardRanking = () => {
     const idUser = localStorage.getItem("idUserLogin")
     let rankingOrder
     const [ranking, setRanking] = useState()
-
+    const [rank, setRank] = useState()
+    const [challengesCompleted, setChallengesCompleted] = useState()
     const datosRanking = async (state) => {
 
         const historyRef = await getDocs(collection(db, "History"))
@@ -55,7 +56,14 @@ const DashboardRanking = () => {
 
         const array2 = arrayRanking.sort(functioRara);
         state(array2)
-
+        let cosa 
+        array2.map((e, i = 0) => {
+            if(e.id == idUser){
+                setRank(i+1)
+                setChallengesCompleted(e.contadorRetos)
+            }
+        })
+        
     }
 
     const functioRara = (a, b) => {
@@ -73,18 +81,19 @@ const DashboardRanking = () => {
     }, [])
 
     
+    
     return (
         <div className='mt-12 flex flex-col items-center lg:px-20 px-2  py-20 border mx-auto rounded-3xl shadow-lg bg-white w-11/12'>
             <div className='flex flex-col lg:flex-row w-full items-center justify-around'>
 
                 <div className='flex flex-col items-center w-full'>
                     <h1 className='font-extralight text-4xl text-gray-800'>Your Rank</h1>
-                    <h1 className='text-[50px]'>#450</h1>
+                    <h1 className='text-[50px] mb-2'>#{rank? rank: "you have no rank"}</h1>
                     <div className='-mt-20'>
                         <img src="https://res.cloudinary.com/dzsd7vfjr/image/upload/v1662170285/Demdoay/platinum_fl3uny.png" alt="" />
                     </div>
                     <h1 className='text-xl'>Number Challenges Completed</h1>
-                    <h1 className='text-[50px]'>150</h1>
+                    <h1 className='text-[50px]'>{challengesCompleted? challengesCompleted: 0}</h1>
 
                 </div>
 
@@ -92,7 +101,7 @@ const DashboardRanking = () => {
                     <h1 className='font-extralight italic text-4xl'>Top Rank</h1>
                     <span className='text-[40px] lg:text-[40px] font-bold text-primary not-italic'>Challenge</span>
                     <GiTrophy size={100} color='#f7d413' />
-                    <div className='divTable w-full mt-10 text-center'>
+                    <div className='divTable w-full mt-10  text-center overflow-y-scroll'>
                         <TableContainer>
                             <Table size='lg'>
                                 <Thead>
@@ -110,7 +119,7 @@ const DashboardRanking = () => {
                                             <Tr key={idx}>
                                                 <Td>{e.name}</Td>
                                                 <Td>{e.contadorCalorias}</Td>
-                                                <Td>{e.contadorTiempo}</Td>
+                                                <Td>{e.contadorTiempo} Minutes</Td>
                                                 <Td>{e.contadorRetos}</Td>
                                             </Tr>
                                         ))
